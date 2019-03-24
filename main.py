981eb1ebd916b412f18b683f794f4e846c3d8ea4
tsmaddox15 <tsmaddox15@gmail.com>
2019-03-24 17:29:10 -0400

    added reading files

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import gym
+import Output
+import sys
+
+
+class Tee(object):
+    def __init__(self, *files):
+        self.files = files
+
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush()  # If you want the output to be visible immediately
+
+    def flush(self):
+        for f in self.files:
+            f.flush()
+
+
 env = gym.make('CartPole-v0')
-for i_episode in range(2000):
+for i_episode in range(10):
     observation = env.reset()
     for t in range(100):
         env.render()
@@ -8,6 +26,16 @@ for i_episode in range(2000):
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print("Episode finished after {} timesteps".format(t + 1))
             break
 env.close()
+# Tee = Output.Tee()
+f = open('out.txt', 'w')
+original = sys.stdout
+sys.stdout = Tee(sys.stdout, f)
+print(sys.stdout)  # This will go to stdout and the file out.txt
+
+# use the original
+sys.stdout = original
+print("This won't appear on file")  # Only on stdout
+f.close()
